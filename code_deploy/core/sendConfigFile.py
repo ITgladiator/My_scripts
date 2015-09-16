2a2d0a25551a52dfad634e91cc3af8fca3866a5b
ITgladiator <506226866@qq.com>
2015-09-16 14:32:34 +0800

    1

--- code_deploy/core/sendConfigFile.py
+++ code_deploy/core/sendConfigFile.py
@@ -2,12 +2,14 @@
 # encoding:utf-8
 
 import commands
+import re
 
 
 def sendConfig(nodeName, siteName):
     cmd = "salt \'" + nodeName + "\' state.sls " + siteName + "_config." + siteName + "_config env=prod"
     status, result = commands.getstatusoutput(cmd)
-    if status == 0:
+    a = re.findall("Failed:    0", result)
+    if len(a) != 0:
         print "\033[33;1mOK! Send the config file sucessfull!\033[0m"
     else:
         print "\033[31;1mERROR! Send the config file failed!\033[0m"
