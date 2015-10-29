f7813d7986a917c715a695274d8e9139ebcf3771
ITgladiator <506226866@qq.com>
2015-10-29 13:03:07 +0800

    修改命令结果判断方法

--- code_deploy/core/sendConfigFile.py
+++ code_deploy/core/sendConfigFile.py
@@ -2,17 +2,19 @@
 # encoding:utf-8
 
 import commands
-import re
 
 
 def sendConfig(nodeName, siteName):
     cmd = "salt '%s' state.sls %s_config.%s_config env=prod" % (nodeName, siteName, siteName)
     status, result = commands.getstatusoutput(cmd)
-    a = re.findall("Failed:    0", result)
-    if len(a) != 0:
-        print "\033[32;1mOK! Send the config file to %s sucessfull!\033[0m" % nodeName
-    else:
-        print "\033[31;1mERROR! Send the config file to %s failed!\033[0m" % nodeName
+    result_list = result.split('\n')
+    for item in result_list:
+        if "Failed" in item:
+            item_list = item.split()
+            if item_list[1] == '0':
+                print "\033[32;1mOK! Send the config file to %s sucessfull!\033[0m" % nodeName
+            else:
+                print "\033[31;1mERROR! Send the config file to %s failed!\033[0m" % nodeName
 
 
 if __name__ == "__main__":
