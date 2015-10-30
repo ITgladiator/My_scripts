5944c9520c7ba6fa9f932531ccbfb12fe230290c
ITgladiator <506226866@qq.com>
2015-10-30 10:44:48 +0800

    添加多项目删除

--- code_deploy/delete_old_version.py
+++ code_deploy/delete_old_version.py
@@ -5,12 +5,22 @@ import commands
 import sys
 from conf import setting
 
-host = "10.10.10.30"
+
+def usage():
+    print "Usage: %s IP PROJECTNAME" % sys.argv[0]
+
+
+if len(sys.argv) != 3:
+    usage()
+    sys.exit()
+
+host = sys.argv[1]
+projectName = sys.argv[2]
 user = "root"
 prod_tmp = setting.deploy_config["prod_tmp"]
-log_file = setting.log_file
+log_file = "%s/%s_svn_ver.log" % (setting.log_dir, projectName)
 
-cmd_ls = "ssh %s@%s ls %s |awk -F '_' '{print $3}'|sort -n |uniq" % (user, host, prod_tmp)
+cmd_ls = "ssh %s@%s ls %s | grep %s |awk -F '_' '{print $3}'|sort -n |uniq" % (user, host, prod_tmp, projectName)
 
 f = file(log_file, 'r')
 v_str = ""
@@ -24,22 +34,26 @@ v_now = int(v_str)
 
 status, values = commands.getstatusoutput(cmd_ls)
 v_list = values.split("\n")
+print v_list
 
 # 只保留当前版本和上一个版本
-if len(v_list) <= 2:
+if len(v_list) <= 3:
     print "Nothing to do, exit!"
     print v_list
     sys.exit()
 else:
     v_list.remove(str(v_now))
-    print v_list
-    max = 0
-    for i in v_list:
-        if int(i) > max:
-            max = int(i)
-    v_list.remove(str(max))
+    # print v_list
+    # max = 0
+    # for i in v_list:
+    #     if int(i) > max:
+    #         max = int(i)
+    # v_list.remove(str(max))
+    for i in range(2):
+        v_list.remove(max(v_list))
+        print v_list
     for value in v_list:
-        cmd = "ssh %s@%s mv %s/*_%s_* /tmp/" % (user, host, prod_tmp, value)
+        cmd = "ssh %s@%s mv %s/*_%s_%s_* /tmp/" % (user, host, prod_tmp, projectName, value)
         print cmd
         status, output = commands.getstatusoutput(cmd)
         if status == 0:
